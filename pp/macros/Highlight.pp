cafd7fe5e032fa9062f4edb338d2896474978cfa
tajmone <tajmone@libero.it>
2017-04-14 16:40:43 +0200

    "Highlight.pp" v1.1 (BOM Fix)
    Add  "--validate-input" option to Highlight invocation  (ie: test if input is text, and remove Unicode BOM). This prevents BOM chars from showing up in the highlighted code when importing a file that has BOM.

--- pp/macros/Highlight.pp
+++ pp/macros/Highlight.pp
@@ -1,6 +1,6 @@
 !comment(Highlight pp-macros set)
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-"Highlight.pp" v1.0.1 (2017-04-09)
+"Highlight.pp" v1.1 (2017-04-14)
 
 A set of macros for integrating André Simon's Highlight syntax highlighter with
 pandoc documents:
@@ -17,7 +17,7 @@ REQUIREMENTS: Highlight cli tool must be available on system %PATH%.
 
 !define(HighlightFile)(
 
-<pre class="hl"><code class="\2">!cmd[highlight.exe -f -S \2 --no-trailing-nl !ifdef(3)(\3) \1]</code></pre>
+<pre class="hl"><code class="\2">!cmd[highlight.exe -f -S \2 --no-trailing-nl --validate-input !ifdef(3)(\3) \1]</code></pre>
 
 )
 
